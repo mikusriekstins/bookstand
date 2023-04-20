40a7dc623a914bf196c16f4f727e325e0497f8f4
Mikus Riekstins <mikus@standardsdigital.com>
2023-04-20 22:25:33 +0300

    Tag sidebar

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -1,7 +1,24 @@
 import styles from './Tag.module.scss'
 
-const Tag = (props: any) => {
-  return <a href='#' className={ styles.tag }>{ props.children }</a>
+interface TagProps {
+  type?: "default" | "block",
+  href?: string,
+  className?: string,
+  children?: any,
+}
+
+const Tag = (props: TagProps) => {
+  const {
+    type,
+    href,
+    className,
+    children
+  } = props
+
+  let tagClass = styles.inlineTag
+  if (type === "block") tagClass = styles.blockTag
+
+  return <a href={ props.href } className={ tagClass }>{ props.children }</a>
 }
 
 export default Tag
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,62 @@
-import type { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next'
+import type { 
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  NextPage
+} from 'next'
 import React, { useEffect } from 'react'
-import { useQuery } from 'react-query'
+//import { useQuery } from 'react-query'
 
 import Bookmark from '../components/Bookmark'
 import Search from '../components/Search'
+import Tag from '../components/Tag'
 import type { BookmarkType } from '../components/Bookmark/Bookmark'
 
 export const getServerSideProps: GetServerSideProps<{ bookmarks: BookmarkType[] }> = async (context) => {
   const bookmarksRes = await fetch(process.env.API_ENDPOINT + "posts/all/" + process.env.API_KEY + "&format=json&results=1000")
-  const data = await bookmarksRes.json()
+  const tagsRes = await fetch(process.env.API_ENDPOINT + "tags/get/" + process.env.API_KEY + "&format=json")
+  const bookmarkData = await bookmarksRes.json()
+  const tagsData = await tagsRes.json()
 
   return {
     props: {
-      bookmarks: data,
+      bookmarks: bookmarkData,
+      tags: tagsData
     },
   }
 }
+
 const Home: NextPage = (props: any) => {
-  const { bookmarks, ...rest } = props;
-  const [searchPhrase, setSearchPhrase] = React.useState('')
-  const [bookmarkList, setBookmarkList] = React.useState(bookmarks)
+  const { bookmarks, tags, ...rest } = props;
+  const [ searchPhrase, setSearchPhrase ] = React.useState('')
+  const [ bookmarkList, setBookmarkList ] = React.useState(bookmarks)
+  const [ tagList, setTagList ] = React.useState(Object.keys(tags))
 
   useEffect(() => {
-    const filteredList = bookmarks.filter((bookmark:BookmarkType) => {
+    const filteredBookmarks = bookmarks.filter((bookmark:BookmarkType) => {
       if (searchPhrase.length < 1) {
-        console.log("nothing")
         return bookmark
       }
 
       if (bookmark.description.toLowerCase().includes(searchPhrase.toLowerCase())) {
-        console.log("something")
         return bookmark
       }
     })
 
-    setBookmarkList(filteredList)
-  }, [ searchPhrase, bookmarks ])
+    const tagKeys = Object.keys(tags)
+    const filteredTags = tagKeys.filter((tag:string) => {
+      if (searchPhrase.length < 1) {
+        return tag
+      }
+
+      if (tag.toLowerCase().includes(searchPhrase.toLowerCase())) {
+        return tag
+      }
+    })
+
+    setBookmarkList(filteredBookmarks)
+    setTagList(filteredTags)
+  }, [ searchPhrase, bookmarks, tags ])
 
   const handleSearch = (event:React.ChangeEvent<HTMLInputElement>) => {
     setSearchPhrase(event.target.value)
@@ -45,9 +66,18 @@ const Home: NextPage = (props: any) => {
     <div className='container'>
       <h1>All Bookmarks</h1>
       <Search onChange={ (e) => { handleSearch(e) } } />
-      { bookmarkList.map((bookmark: BookmarkType, key: number) => {
-        return <Bookmark key={ key } data={ bookmark } />
-      }) }
+      <div className='grid'>
+        <div className='col col--xl-3'>
+          { tagList.map((tag: string, key: number) => {
+            return <Tag type="block" key={ key } href={ "/tag/" + tag }>{ tag }</Tag>
+          }) }
+        </div>
+        <div className='col col--xl-9'>
+          { bookmarkList.map((bookmark: BookmarkType, key: number) => {
+            return <Bookmark key={ key } data={ bookmark } />
+          }) }
+        </div>
+      </div>
     </div>
   )
 }
